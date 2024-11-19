956e0f36440072d95c3a5b2d38553f8ff6a21419
Al Ibne Siam <ibne.siam@northsouth.edu>
2024-11-19 20:53:25 +0600

    authentication implemented

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegiserForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -8,8 +9,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}')
-            return redirect('blog-home')
+            messages.success(request, f'Account created for {username}. Please login to your account.')
+            return redirect('login')
     else:
         form = UserRegiserForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
