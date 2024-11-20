0f4922b542bf581e70498ea40c73001f8ebb77a2
Al Ibne Siam <ibne.siam@northsouth.edu>
2024-11-20 12:20:24 +0600

    profile updater implemented

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
+MAX_IMG_DIM = 256
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,3 +10,11 @@ class Profile(models.Model):
     
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
+    
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        img.thumbnail((256,256))
+        img.save(self.image.path)
+        
+        
